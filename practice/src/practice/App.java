c5876a0875a23c9c4d4849119811ed5df44ed2d6
Vishal Koli <vishalkoli1402@gmail.com>
2026-02-08 23:06:44 +0530

    "binarySearch method added"

--- practice/src/practice/App.java
+++ practice/src/practice/App.java
@@ -1,7 +1,14 @@
 package practice;
 
+import java.util.Arrays;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("JAVA");
+		
+		Demo d = new Demo();
+		int[] arr = {1,2,3,4,5,6,9,10};
+		Arrays.sort(arr);
+		System.out.println(d.binarySearch(arr, 12));
+
 	}
 }
--- practice/src/practice/Demo.java
+++ practice/src/practice/Demo.java
@@ -1,5 +1,25 @@
 package practice;
 
 public class Demo {
+	
+	
+	
 
+	
+	int binarySearch(int [] arr,int target) {
+		int start = 0,end = arr.length-1;
+		while(start<=end) {
+			int mid = start + (end-start)/2;
+			
+			if(arr[mid]==target) {
+				return mid;
+			}else if(arr[mid]<target) {
+				start = mid+1;
+			}else {
+				end = mid-1;
+			}
+		}
+		
+		return -1;
+	}
 }
